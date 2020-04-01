14712c84efbb58a645893554fab8c2bdf92ef061
ihor-doroschenko <igordoroshenko@outlook.com>
2020-04-01 16:47:38 +0200

    Updates

---
+++ precache-manifest.343979bcf8e5afda387d9c850cd54d3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b157a08a310766835741a290c8f37fe",
+    "url": "/ihor-doroschenko/cam-cube/index.html"
+  },
+  {
+    "revision": "500838fbbbc6b70c3629",
+    "url": "/ihor-doroschenko/cam-cube/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8dccda4d2361e3c7edd8",
+    "url": "/ihor-doroschenko/cam-cube/static/js/2.dca53e12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ihor-doroschenko/cam-cube/static/js/2.dca53e12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "500838fbbbc6b70c3629",
+    "url": "/ihor-doroschenko/cam-cube/static/js/main.7fb8af66.chunk.js"
+  },
+  {
+    "revision": "f7c980eea724275ce989",
+    "url": "/ihor-doroschenko/cam-cube/static/js/runtime-main.8820b630.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ihor-doroschenko/cam-cube/static/media/logo.5d5d9eef.svg"
+  }
+]);
